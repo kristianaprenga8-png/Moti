4a6c7d7fc5990edd950530eb3b6f5d2e2cf9e58f
kristianaprenga8-png <kristianaprenga8@gmail.com>
2026-06-07 23:35:03 +0200

    new function for time and icons

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ['Sun' , 'Mon' ,'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+  return days[date.getDay()];
+}
+
+
 function getForecast (city){
   let apiKey ="66db02e29ab4305o5a5c75fc1b23aet8";
   let apiUrl ='https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}$units=metric';
@@ -67,21 +75,23 @@ function displayForecast() {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function(day, index) {
+    if (index < 5) {
     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src= "${day.condition.icon_url}" class="weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}15º</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round (day.temperature.mimimum)}º</div>
         </div>
       </div>
     `;
+    } 
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -93,4 +103,3 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Berlin");
 getForecast("Berlin");
-
